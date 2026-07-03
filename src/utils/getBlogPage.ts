9066a5a7423fe9da588000e71b95640d43067aeb
DaR3kDev <villegasdeveloper@outlook.es>
2026-07-03 16:12:55 -0500

    style(getBlogPage): clean up and remove debug logs
    - replace double quotes with single quotes throughout the file
    - standardize indentation for consistent code formatting
    - simplify missing page error handling

--- src/utils/getBlogPage.ts
+++ src/utils/getBlogPage.ts
@@ -15,23 +15,12 @@ export async function getPageData<C extends CollectionName>(
 	const lang = getLangFromUrl(url)
 	const pages = await getCollection(collection)
 
-	console.log("Collection:", collection)
-	console.log("Lang:", lang)
-	console.log(
-		"Ids:",
-		pages.map((page) => page.id),
-	)
-
 	const page = pages.find((page) => {
 		const pageLang = page.id.split("/")[0]
 		return pageLang === lang
 	})
 
-	console.log("Page encontrada:", page)
-
-	if (!page) {
-		throw new Error(`No existe contenido para la colección "${collection}" para el idioma "${lang}".`)
-	}
+	if (!page) throw new Error(`No existe contenido para la colección "${collection}" para el idioma "${lang}".`)
 
 	return page.data
 }
